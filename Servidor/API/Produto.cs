c2bce831cb89d0c8ac6360a8569afc9a873fbc88
H4XOR98 <lazaropinheiro@MBP-de-Lazaro.lan>
2020-03-09 21:18:41 +0000

    Hx - Classes
    Commit of the main classes

---
+++ Servidor/API/Produto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Drawing;
+
+namespace API
+{ 
+    public class Produto
+    {
+        public int IdProduto { get; }
+	    public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public double Preco { get; set; }
+        public IList<string> Ingredientes { get; set; }
+    	public IList<string> Alergenios { get; set; }
+	    public Bitmap Fotografia { get; set; }
+
+
+        private static int id = 0;
+
+
+        public Produto(string Nome, string Categoria, double Preco, IList<string> Ingredientes, IList<string> Alergenios, Bitmap Fotografia)
+        {
+            this.IdProduto = id++;
+            this.Nome = Nome;
+            this.Categoria = Categoria;
+            this.Preco = Preco;
+            this.Ingredientes = Ingredientes;
+            this.Alergenios = Alergenios;
+            this.Fotografia = Fotografia;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if(obj == this)
+            {
+                return true;
+            }
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            Produto produto = (Produto)obj;
+            return IdProduto == produto.IdProduto;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Produto\n");
+            sb.Append("- ID : " + IdProduto + "\n");
+            sb.Append("- Nome : " + Nome + "\n");
+            sb.Append("- Categoria : " + Categoria + "\n");
+            sb.Append("- Preco : " + Preco + "\n");
+            sb.Append("- Ingredientes: ");
+            foreach(var ingrediente in Ingredientes)
+            {
+                sb.Append(ingrediente + "; ");
+            }
+            sb.Append("\n- Alergénios: ");
+            foreach (var alergenio in Alergenios)
+            {
+                sb.Append(alergenio + "; ");
+            }
+            sb.Append("\n");
+            return sb.ToString();
+        }
+
+    }
+}
