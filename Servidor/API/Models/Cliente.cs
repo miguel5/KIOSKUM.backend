3e4db5e56634510ecf8ee6278dac5b28aff94197
H4XOR98 <lazaropinheiro@MBP-de-Lazaro.lan>
2020-03-14 02:25:52 +0000

    Validate NumTelemovel

--- Servidor/API/Models/Cliente.cs
+++ Servidor/API/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace API.Models
 {
@@ -12,9 +13,20 @@ namespace API.Models
         public Cliente(int IdUtilizador, string Nome, string Email, string Password, int NumTelemovel) : base(IdUtilizador, Nome)
         {
             this.Email = Email;
-            this.Password = Password;
+            this.Password = HashPassword(Password);
             this.NumTelemovel = NumTelemovel;
         }
+
+        private string HashPassword(string password)
+        {
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: new byte[0],
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+        }
+
 
         public override int GetHashCode()
         {
