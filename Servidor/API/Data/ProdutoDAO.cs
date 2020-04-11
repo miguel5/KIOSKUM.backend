f76eb161e0da2e41633c7946a633371325bc8890
Sapos1 <emanuel99@sapo.pt>
2020-04-11 14:53:41 +0100

    Inicio ProdutoDAO

--- Servidor/API/Data/ProdutoDAO.cs
+++ Servidor/API/Data/ProdutoDAO.cs
@@ -25,7 +25,25 @@ namespace API.Data
 
         bool IProdutoDAO.ExisteNomeProduto(string nome)
         {
-            throw new NotImplementedException();
+            MySqlCommand cmd;
+            if (_connectionDB.OpenConnection())
+            {
+                cmd = new MySqlCommand();
+                cmd.Connection = _connectionDB.Connection;
+
+                cmd.CommandText = "existe_nome_produto";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("?nome", nome);
+                cmd.Parameters["?nome"].Direction = ParameterDirection.Input;
+
+                object val = cmd.ExecuteScalar();
+                
+                _connectionDB.CloseConnection();
+                
+                return Convert.ToBoolean(val);
+            }
+            return false;
         }
 
         public void AddProduto(Produto produto)
@@ -35,17 +53,69 @@ namespace API.Data
 
         public IList<Produto> GetProdutos(int idCategoria)
         {
-            throw new NotImplementedException();
+            MySqlCommand cmd;
+            if (_connectionDB.OpenConnection())
+            {
+                cmd = new MySqlCommand();
+                cmd.Connection = _connectionDB.Connection;
+
+                cmd.CommandText = "get_produtos";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("?idCategoria", idCategoria);
+                cmd.Parameters["?idCategoria"].Direction = ParameterDirection.Input;
+
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                
+                try
+                {
+                    while (rdr.Read())
+                    {
+                    }
+                }
+                catch (Exception) { }
+                finally
+                {
+                    _connectionDB.CloseConnection();
+                }
+            }
+            return null;
         }
 
         public void EditarProduto(Produto produto)
         {
-            throw new NotImplementedException();
+            MySqlCommand cmd;
+            if (_connectionDB.OpenConnection())
+            {
+                cmd = new MySqlCommand();
+                cmd.Connection = _connectionDB.Connection;
+
+                cmd.CommandText = "editar_produto";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("?id", produto.IdProduto);
+                cmd.Parameters["?id"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("?nome", produto.Nome);
+                cmd.Parameters["?nome"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("?preco", produto.Preco);
+                cmd.Parameters["?preco"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("?entensao_imagem", produto.ExtensaoImagem);
+                cmd.Parameters["?entensao_imagem"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("?categoria", produto.IdCategoria);
+                cmd.Parameters["?categoria"].Direction = ParameterDirection.Input;
+
+                cmd.ExecuteNonQuery();
+
+                _connectionDB.CloseConnection();
+            }
         }
 
         public Produto GetProdutoNome(string nome)
         {
-            throw new NotImplementedException();
         }
     }
 }
