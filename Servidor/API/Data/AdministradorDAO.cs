487be0ae145e5fa7c0c7920536e3dce58a037e25
Sapos1 <emanuel99@sapo.pt>
2020-05-25 17:17:39 +0100

    funcionario & admin

--- Servidor/API/Data/AdministradorDAO.cs
+++ Servidor/API/Data/AdministradorDAO.cs
@@ -15,32 +15,157 @@ namespace API.Data
 
         public void EditarConta(Administrador administrador)
         {
-            throw new System.NotImplementedException();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = _connectionDBService.Connection;
+
+            cmd.CommandText = "editar_conta_admin";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("?id", administrador.IdFuncionario);
+            cmd.Parameters["?id"].Direction = ParameterDirection.Input;
+
+            cmd.Parameters.AddWithValue("?nome", administrador.Nome);
+            cmd.Parameters["?nome"].Direction = ParameterDirection.Input;
+
+            cmd.Parameters.AddWithValue("?numero", administrador.NumFuncionario);
+            cmd.Parameters["?numero"].Direction = ParameterDirection.Input;
+
+            cmd.Parameters.AddWithValue("?mail", administrador.Email);
+            cmd.Parameters["?mail"].Direction = ParameterDirection.Input;
+
+            cmd.Parameters.AddWithValue("?pass", administrador.Password);
+            cmd.Parameters["?pass"].Direction = ParameterDirection.Input;
+
+            cmd.ExecuteNonQuery();
+
+            _connectionDBService.CloseConnection();
         }
 
         public bool ExisteEmail(string email)
         {
-            throw new System.NotImplementedException();
+            _connectionDBService.OpenConnection();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = _connectionDBService.Connection;
+
+            cmd.CommandText = "existe_mail_admin";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("?mail", email);
+            cmd.Parameters["?mail"].Direction = ParameterDirection.Input;
+
+            object val = cmd.ExecuteScalar();
+
+            _connectionDBService.CloseConnection();
+
+            return Convert.ToBoolean(val);
         }
 
         public bool ExisteNumFuncionario(int numFuncionario)
         {
-            throw new System.NotImplementedException();
+            _connectionDBService.OpenConnection();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = _connectionDBService.Connection;
+
+            cmd.CommandText = "existe_num_funcionario";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("?numero", numFuncionario);
+            cmd.Parameters["?numero"].Direction = ParameterDirection.Input;
+
+            object val = cmd.ExecuteScalar();
+
+            _connectionDBService.CloseConnection();
+
+            return Convert.ToBoolean(val);
         }
 
         public Administrador GetContaEmail(string email)
         {
-            throw new System.NotImplementedException();
+            _connectionDBService.OpenConnection();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = _connectionDBService.Connection;
+
+            cmd.CommandText = "get_admin_mail";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("?mail", email);
+            cmd.Parameters["?mail"].Direction = ParameterDirection.Input;
+
+            MySqlDataReader var = cmd.ExecuteReader();
+
+            Administrador administrador = null;
+            try
+            {
+                if (var.Read())
+                {
+                    administrador = new Administrador { IdFuncionario = var.GetInt32(0), Nome = var.GetString(1), NumFuncionario = var.GetInt32(2), Email = email, Password = var.GetString(3)};
+                }
+                return funcionario;
+            }
+            catch (Exception) { throw; }
+            finally
+            {
+                _connectionDBService.CloseConnection();
+            }
         }
 
         public Administrador GetContaId(int idFuncionario)
         {
-            throw new System.NotImplementedException();
+            _connectionDBService.OpenConnection();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = _connectionDBService.Connection;
+
+            cmd.CommandText = "get_admin_id";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("?id", idFuncionario);
+            cmd.Parameters["?id"].Direction = ParameterDirection.Input;
+
+            MySqlDataReader var = cmd.ExecuteReader();
+
+            Administrador administrador = null;
+            try
+            {
+                if (var.Read())
+                {
+                    administrador = new Administrador { IdFuncionario = idFuncionario, Nome = var.GetString(0), NumFuncionario = var.GetInt32(1), Email = var.GetString(2), Password = var.GetString(3)};
+                }
+                return funcionario;
+            }
+            catch (Exception) { throw; }
+            finally
+            {
+                _connectionDBService.CloseConnection();
+            }
         }
 
         public void InserirConta(Administrador administrador)
         {
-            throw new System.NotImplementedException();
+            _connectionDBService.OpenConnection();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = _connectionDBService.Connection;
+
+            cmd.CommandText = "inserir_admin";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("?nome", administrador.Nome);
+            cmd.Parameters["?nome"].Direction = ParameterDirection.Input;
+
+            cmd.Parameters.AddWithValue("?numero", administrador.NumFuncionario);
+            cmd.Parameters["?numero"].Direction = ParameterDirection.Input;
+
+            cmd.Parameters.AddWithValue("?mail", administrador.Email);
+            cmd.Parameters["?mail"].Direction = ParameterDirection.Input;
+
+            cmd.Parameters.AddWithValue("?pass", administrador.Password);
+            cmd.Parameters["?pass"].Direction = ParameterDirection.Input;
+
+            cmd.ExecuteNonQuery();
+
+            _connectionDBService.CloseConnection();
         }
     }
 }
--- Servidor/API/Data/FuncionarioDAO.cs
+++ Servidor/API/Data/FuncionarioDAO.cs
@@ -15,24 +15,97 @@ namespace API.Data
             _connectionDBService = connectionDBService;
         }
 
-        public void EditarNome(string nome)
+        public void EditarConta(Funcionario funcionario)
         {
-            throw new NotImplementedException();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = _connectionDBService.Connection;
+
+            cmd.CommandText = "editar_conta_funcionario";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("?id", funcionario.IdFuncionario);
+            cmd.Parameters["?id"].Direction = ParameterDirection.Input;
+
+            cmd.Parameters.AddWithValue("?nome", funcionario.Nome);
+            cmd.Parameters["?nome"].Direction = ParameterDirection.Input;
+
+            cmd.Parameters.AddWithValue("?numero", funcionario.NumFuncionario);
+            cmd.Parameters["?numero"].Direction = ParameterDirection.Input;
+
+            cmd.ExecuteNonQuery();
+
+            _connectionDBService.CloseConnection();
         }
 
         public bool ExisteNumFuncionario(int numFuncionario)
         {
-            throw new NotImplementedException();
+            _connectionDBService.OpenConnection();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = _connectionDBService.Connection;
+
+            cmd.CommandText = "existe_num_funcionario";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("?numero", numFuncionario);
+            cmd.Parameters["?numero"].Direction = ParameterDirection.Input;
+
+            object val = cmd.ExecuteScalar();
+
+            _connectionDBService.CloseConnection();
+
+            return Convert.ToBoolean(val);
         }
 
         public Funcionario GetContaNumFuncionario(int numFuncionario)
         {
-            throw new NotImplementedException();
+            _connectionDBService.OpenConnection();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = _connectionDBService.Connection;
+
+            cmd.CommandText = "get_funcionario_numero";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("?numero", numFuncionario);
+            cmd.Parameters["?numero"].Direction = ParameterDirection.Input;
+
+            MySqlDataReader var = cmd.ExecuteReader();
+
+            Funcionario funcionario = null;
+            try
+            {
+                if (var.Read())
+                {
+                    funcionario = new Funcionario { IdFuncionario = var.GetInt32(0), Nome = var.GetString(1), NumFuncionario = numFuncionario };
+                }
+                return funcionario;
+            }
+            catch (Exception) { throw; }
+            finally
+            {
+                _connectionDBService.CloseConnection();
+            }
         }
 
         public void InserirConta(Funcionario funcionario)
         {
-            throw new NotImplementedException();
+            _connectionDBService.OpenConnection();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = _connectionDBService.Connection;
+
+            cmd.CommandText = "inserir_funcionario";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("?nome", funcionario.Nome);
+            cmd.Parameters["?nome"].Direction = ParameterDirection.Input;
+
+            cmd.Parameters.AddWithValue("?numero", funcionario.NumFuncionario);
+            cmd.Parameters["?numero"].Direction = ParameterDirection.Input;
+
+            cmd.ExecuteNonQuery();
+
+            _connectionDBService.CloseConnection();
         }
     }
 }
