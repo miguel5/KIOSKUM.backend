90b45a210601bae89c6d0a93e71af0424d9cc631
Lázaro Pinheiro <47797368+H4XOR98@users.noreply.github.com>
2020-07-03 00:00:40 +0100

    Commit (#117)
    Co-authored-by: H4XOR98 <lazaropinheiro@MBP-de-Lazaro.lan>

--- Servidor/DAO/AdministradorDAO.cs
+++ Servidor/DAO/AdministradorDAO.cs
@@ -21,31 +21,6 @@ namespace DAO
 
         public void EditarConta(Administrador administradorEditado)
         {
-            throw new NotImplementedException();
-        }
-
-        public bool ExisteNumFuncionario(int numFuncionario)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Administrador GetContaIdFuncionario(int idFuncionario)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Administrador GetContaNumFuncionario(int numFuncionario)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void InserirConta(Administrador administrador)
-        {
-            throw new NotImplementedException();
-        }
-
-        /*public void EditarConta(Administrador administrador)
-        {
             _logger.LogDebug("A executar [AdministradorDAO -> EditarConta]");
             try
             {
@@ -57,19 +32,16 @@ namespace DAO
                     cmd.CommandText = "editar_conta_admin";
                     cmd.CommandType = CommandType.StoredProcedure;
 
-                    cmd.Parameters.AddWithValue("?id", administrador.IdFuncionario);
+                    cmd.Parameters.AddWithValue("?id", administradorEditado.IdFuncionario);
                     cmd.Parameters["?id"].Direction = ParameterDirection.Input;
 
-                    cmd.Parameters.AddWithValue("?nome", administrador.Nome);
+                    cmd.Parameters.AddWithValue("?nome", administradorEditado.Nome);
                     cmd.Parameters["?nome"].Direction = ParameterDirection.Input;
 
-                    cmd.Parameters.AddWithValue("?numero", administrador.NumFuncionario);
+                    cmd.Parameters.AddWithValue("?numero", administradorEditado.NumFuncionario);
                     cmd.Parameters["?numero"].Direction = ParameterDirection.Input;
 
-                    cmd.Parameters.AddWithValue("?mail", administrador.Email);
-                    cmd.Parameters["?mail"].Direction = ParameterDirection.Input;
-
-                    cmd.Parameters.AddWithValue("?pass", administrador.Password);
+                    cmd.Parameters.AddWithValue("?pass", administradorEditado.Password);
                     cmd.Parameters["?pass"].Direction = ParameterDirection.Input;
 
                     cmd.ExecuteNonQuery();
@@ -83,36 +55,9 @@ namespace DAO
             }
         }
 
-        public bool ExisteEmail(string email)
-        {
-            _logger.LogDebug("A executar [AdministradorDAO -> ExisteEmail]");
-            try
-            {
-                _connectionDBService.OpenConnection();
-                using (MySqlCommand cmd = new MySqlCommand())
-                {
-                    cmd.Connection = _connectionDBService.Connection;
-
-                    cmd.CommandText = "existe_mail_admin";
-                    cmd.CommandType = CommandType.StoredProcedure;
-
-                    cmd.Parameters.AddWithValue("?mail", email);
-                    cmd.Parameters["?mail"].Direction = ParameterDirection.Input;
-
-                    object val = cmd.ExecuteScalar();
-
-                    return Convert.ToBoolean(val);
-                }
-            }
-            catch { throw; }
-            finally
-            {
-                _connectionDBService.CloseConnection();
-            }
-        }
-
         public bool ExisteNumFuncionario(int numFuncionario)
         {
+            _logger.LogDebug("A executar [FuncionarioDAO -> ExisteNumFuncionario]");
             try
             {
                 _connectionDBService.OpenConnection();
@@ -138,9 +83,9 @@ namespace DAO
             }
         }
 
-        public Administrador GetContaEmail(string email)
+        public Administrador GetContaIdFuncionario(int idFuncionario)
         {
-            _logger.LogDebug("A executar [AdministradorDAO -> GetContaEmail]");
+            _logger.LogDebug("A executar [AdministradorDAO -> GetContaIdFuncionario]");
             try
             {
                 _connectionDBService.OpenConnection();
@@ -148,43 +93,7 @@ namespace DAO
                 {
                     cmd.Connection = _connectionDBService.Connection;
 
-                    cmd.CommandText = "get_admin_mail";
-                    cmd.CommandType = CommandType.StoredProcedure;
-
-                    cmd.Parameters.AddWithValue("?mail", email);
-                    cmd.Parameters["?mail"].Direction = ParameterDirection.Input;
-
-                    using (MySqlDataReader var = cmd.ExecuteReader())
-                    {
-
-                        Administrador administrador = null;
-                        if (var.Read())
-                        {
-                            administrador = new Administrador { IdFuncionario = var.GetInt32(0), Nome = var.GetString(1), NumFuncionario = var.GetInt32(2), Email = email, Password = var.GetString(3) };
-                        }
-
-                        return administrador;
-                    }
-                }
-            }
-            catch (Exception) { throw; }
-            finally
-            {
-                _connectionDBService.CloseConnection();
-            }
-        }
-
-        public Administrador GetContaId(int idFuncionario)
-        {
-            _logger.LogDebug("A executar [AdministradorDAO -> GetContaId]");
-            try
-            {
-                _connectionDBService.OpenConnection();
-                using (MySqlCommand cmd = new MySqlCommand())
-                {
-                    cmd.Connection = _connectionDBService.Connection;
-
-                    cmd.CommandText = "get_admin_id";
+                    cmd.CommandText = "get_administrador_id";
                     cmd.CommandType = CommandType.StoredProcedure;
 
                     cmd.Parameters.AddWithValue("?id", idFuncionario);
@@ -197,14 +106,43 @@ namespace DAO
 
                         if (var.Read())
                         {
-                            administrador = new Administrador
-                            {
-                                IdFuncionario = idFuncionario,
-                                Nome = var.GetString(0),
-                                NumFuncionario = var.GetInt32(1),
-                                Email = var.GetString(2),
-                                Password = var.GetString(3)
-                            };
+                            administrador = new Administrador { IdFuncionario = idFuncionario, Nome = var.GetString(0), NumFuncionario = var.GetInt32(2), Password = var.GetString(1) };
+                        }
+                        return administrador;
+                    }
+                }
+            }
+            catch (Exception) { throw; }
+            finally
+            {
+                _connectionDBService.CloseConnection();
+            }
+        }
+
+        public Administrador GetContaNumFuncionario(int numFuncionario)
+        {
+            _logger.LogDebug("A executar [AdministradorDAO -> GetContaNumFuncionario]");
+            try
+            {
+                _connectionDBService.OpenConnection();
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = _connectionDBService.Connection;
+
+                    cmd.CommandText = "get_administrador_numero";
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("?numero", numFuncionario);
+                    cmd.Parameters["?numero"].Direction = ParameterDirection.Input;
+
+                    using (MySqlDataReader var = cmd.ExecuteReader())
+                    {
+
+                        Administrador administrador = null;
+
+                        if (var.Read())
+                        {
+                            administrador = new Administrador { IdFuncionario = var.GetInt32(0), Nome = var.GetString(1), NumFuncionario = numFuncionario, Password = var.GetString(2) };
                         }
                         return administrador;
                     }
@@ -236,9 +174,6 @@ namespace DAO
                     cmd.Parameters.AddWithValue("?numero", administrador.NumFuncionario);
                     cmd.Parameters["?numero"].Direction = ParameterDirection.Input;
 
-                    cmd.Parameters.AddWithValue("?mail", administrador.Email);
-                    cmd.Parameters["?mail"].Direction = ParameterDirection.Input;
-
                     cmd.Parameters.AddWithValue("?pass", administrador.Password);
                     cmd.Parameters["?pass"].Direction = ParameterDirection.Input;
 
@@ -251,6 +186,6 @@ namespace DAO
             {
                 _connectionDBService.CloseConnection();
             }
-        }*/
+        }
     }
 }
